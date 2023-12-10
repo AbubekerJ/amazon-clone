8a346e6d91422a2bf7e1ca32992c1f520f1f529b
AbubekerJ <110613188+AbubekerJ@users.noreply.github.com>
2023-12-10 12:11:50 +0000

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -1,32 +1,49 @@
 
 
-// const {onRequest} = require("firebase-functions/v2/https");
-// const logger = require("firebase-functions/logger");
-// const express = require ('express')
-// const cors =require ('cors')
-// const stripe =require('stripe')('sk_test_51O7buhITzAyJfbAGqKxWevglokrrTpPyRTXdFyV2IjrVPiVnlbNTeanLiw8OF1mnJaLYzZ2gg4ah1RumBDDG4UBa00KjSrhj8D')
+
+// const functions = require('firebase-functions'); // Import the 'functions' module.
+
+// const express = require('express');
+// const cors = require('cors');
+// const stripe = require('stripe')(
+
+//     'sk_test_51O7buhITzAyJfbAGqKxWevglokrrTpPyRTXdFyV2IjrVPiVnlbNTeanLiw8OF1mnJaLYzZ2gg4ah1RumBDDG4UBa00KjSrhj8D'
+    
+//     );
+//     const app = express();
+
+//     app.use(cors({ origin: true }));
+//     app.use(express.json());
+    
+//     app.get("/", (request, response) => response.status(200).send("hello world"));
+    
+//     app.post("/payments/create", async (request, response) => {
+//       const total = request.query.total;
+    
+//       console.log("Payment Request Reaceived for this amount >>>>", total);
+    
+//       const paymentIntent = await stripe.paymentIntents.create({
+//         amount: total,
+//         currency: "usd",
+//       });
+//       response.status(201).send({
+//         clientSecret: paymentIntent.client_secret,
+//       });
+//     });
+ 
+//     exports.api = functions.https.onRequest(app);
+
+//     app.listen(5000,(err)=>{
+//       if (err) {
+//         console.log(err.messege)
+//       } else {
+//         console.log("listning to 5000")
+    
+//       }
+//     })
 
 
-// //API CONFIG
-
-// const app = express()
-
-// // API MIDELWARE
-// app.use(cors({origin:true}))
-// app.use (express.json())
-
-// //API ROUTS
-// app.get('/', (req, res)=>{
-
-//     res.status(200).send('HELLO WORLD')
-
-
-// })
-
-// //LISTEEN COMAND
-// exports.api=functions.https.onRequest(app)
-
-
+const axios = require('axios');
 const functions = require('firebase-functions'); // Import the 'functions' module.
 
 const express = require('express');
@@ -36,6 +53,7 @@ const stripe = require('stripe')(
     'sk_test_51O7buhITzAyJfbAGqKxWevglokrrTpPyRTXdFyV2IjrVPiVnlbNTeanLiw8OF1mnJaLYzZ2gg4ah1RumBDDG4UBa00KjSrhj8D'
     
     );
+    const CHAPA_AUTH_KEY = 'CHASECK_TEST-Diu7oee4GKx5cRxJo7PSjzhV8WPyvCmx'
     const app = express();
 
     app.use(cors({ origin: true }));
@@ -56,8 +74,60 @@ const stripe = require('stripe')(
         clientSecret: paymentIntent.client_secret,
       });
     });
- 
-    exports.api = functions.https.onRequest(app);
+
+
+
+
+
+//chapa_payment
+app.post("/accept-payment", async (req, res) => {
+  const {
+    amount,
+    email,
+    first_name,
+    last_name,
+    phone_number,
+    tx_ref,
+    currency, // Add currency to the destructuring
+  } = req.body;
+
+  console.log("Chapa Payment Request Body:", req.body);
+
+  try {
+    const header = {
+      Authorization: `Bearer ${CHAPA_AUTH_KEY}`,
+      "Content-Type": "application/json",
+    };
+
+    const body = {
+      amount: amount,
+      email: email,
+      first_name: first_name,
+      last_name: last_name,
+      phone_number: phone_number,
+      tx_ref: tx_ref,
+      currency: currency, // Include currency in the request body
+      return_url: "http://localhost:3000/", // Set your return URL
+    };
+
+    const response = await fetch("https://api.chapa.co/v1/transaction/initialize", {
+      method: 'POST',
+      headers: header,
+      body: JSON.stringify(body),
+    });
+
+    const responseData = await response.json();
+    console.log("Chapa API Response:", responseData);
+
+    res.status(response.status).json(responseData);
+  } catch (e) {
+    console.error("Chapa API Error:", e);
+    res.status(500).json({
+      error_code: e.code,
+      message: e.message,
+    });
+  }
+});
 
     app.listen(5000,(err)=>{
       if (err) {
@@ -67,3 +137,4 @@ const stripe = require('stripe')(
     
       }
     })
+
